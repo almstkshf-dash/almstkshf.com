6bbf665a6bf0890520963e34db17f92e389da2ad
almstkshfuae <almstkshfuae@gmail.com>
2026-03-11 21:37:08 +0400

    PAGE SPEED INSIGHT

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
+        optimizeCss: true,
         optimizePackageImports: [
             'lucide-react',
             'framer-motion'
